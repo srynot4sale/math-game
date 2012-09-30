8a70976a38e282b6dd7207cd450d3305d8a9389f
Aaron Barnes <aaron@io.net.nz>
2012-09-30 21:38:25 +1300

    Add subtraction, and make the game get progressively more difficult
    Game is easier when first starting, or after an incorrect answer. Then
    it ramps up in difficulty until predefined limits until next incorrect
    answer.

--- play.py
+++ play.py
@@ -11,11 +11,25 @@ class interpreter(cmd.Cmd):
     student = config.student
     people = config.people
 
+    # Next answer
     answer = 0
+
+    # No of correct answers
     count = 0
 
+    # No of incorrect answers
+    incorrect = 0
+
+    # No of correct answers in a row
+    roll = -1
+
+    # Remaining "people"
     current_people = []
 
+    # "No fingers" ceiling
+    no_fingers = 5
+
+    # Answer prompt
     prompt = '= '
 
 
@@ -29,19 +43,57 @@ class interpreter(cmd.Cmd):
 
 
     def question(self):
-        x = random.randint(1, 9)
-        y = 10
-        while x + y > 10:
-            y = random.randint(1, 9)
 
-        self.answer = x + y
+        self.roll += 1
+
+        # 75% change to do addition, or 100% chance if within 5
+        # questions of an incorrect answer (or when first starting)
+        if self.roll < 5 or random.randint(1, 4) > 1:
+            op = '+'
+            mx = 10
+
+            # Grab minimum of two random first numbers, lowers
+            # the amount of higher numbers
+            x1 = random.randint(1, mx - 1)
+            x2 = random.randint(1, mx - 1)
+            x = min(x1, x2)
+
+            y = mx
+
+            # Keep grabbing random second numbers until the add
+            # up to under the mx value
+            # There is also logic so that the questions get
+            # progessively harder up to the mx value after an
+            # incorrect answer (or when first starting)
+            while x + y > mx or (min(x, y) > (self.roll + 1)):
+                y = random.randint(1, mx - 1)
+
+            # Save answer
+            self.answer = x + y
+
+            note = ''
+            if self.answer <= self.no_fingers:
+                note = '(no fingers!)'
+
+        # 25% change to ask subtraction call
+        else:
+            op = '-'
+            x = random.randint(2, 5)
+            y = 3
+            while x - y < 1:
+                y = random.randint(1, 3)
+
+            self.answer = x - y
+            note = ''
+
         print ''
-        print '%d + %d' % (x,y)
+        print '%d %s %d %s' % (x, op, y, note)
         print ''
 
 
     def do_quit(self, data):
         print 'Done %d questions!' % self.count
+        print '%d were incorrect' % self.incorrect
         print 'Good bye %s' % self.student
         return True
 
@@ -56,7 +108,11 @@ class interpreter(cmd.Cmd):
             self.count += 1
 
             if random.randint(1, 5) == 4:
-                p = random.randint(1, len(self.current_people)) - 1
+                if len(self.current_people) == 1:
+                    p = 0
+                else:
+                    p = random.randint(1, len(self.current_people)) - 1
+
                 person = self.current_people[p]
                 del self.current_people[p]
                 text = self.colorize('%s says well done!' % person, 'green')
@@ -70,6 +126,9 @@ class interpreter(cmd.Cmd):
         else:
             print self.colorize('Good try, try again %s :-)' % self.student, 'red')
             print ''
+
+            self.roll = -1
+            self.incorrect += 1
 
 
 
